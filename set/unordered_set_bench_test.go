ed374ad62f7fe391c77f3c8ec7251a9b036f663c
Ryan Kent <25660714+ryan-kent-ci@users.noreply.github.com>
2025-09-06 13:53:03 +0600

    Create unordered_set_bench_test.go

---
+++ set/unordered_set_bench_test.go
@@ -0,0 +1,110 @@
+package set
+
+import (
+	"strconv"
+	"testing"
+)
+
+func BenchmarkUnorderedSet_Insert(b *testing.B) {
+	set := NewUnorderedSet[int]()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		set.Insert(i)
+	}
+}
+
+func BenchmarkUnorderedSet_Contain(b *testing.B) {
+	set := NewUnorderedSet[float32]()
+	for i := 0; i < 10000000; i++ {
+		set.Insert(float32(i))
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		set.Contain(float32(i % 10000000))
+	}
+}
+
+func BenchmarkUnorderedSet_Remove(b *testing.B) {
+	set := NewUnorderedSet[int]()
+	for i := 0; i < b.N; i++ {
+		set.Insert(i)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		set.Remove(i)
+	}
+}
+
+func BenchmarkUnorderedSet_Items(b *testing.B) {
+	set := NewUnorderedSet[int]()
+	for i := 0; i < 100000; i++ {
+		set.Insert(i)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = set.Items()
+	}
+}
+
+func BenchmarkUnorderedSet_StringKeys(b *testing.B) {
+	set := NewUnorderedSet[string]()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		set.Insert(strconv.Itoa(i))
+	}
+}
+
+func BenchmarkUnorderedSet_ParallelInsert(b *testing.B) {
+	set := NewUnorderedSet[int]()
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			set.Insert(i)
+			i++
+		}
+	})
+}
+
+func BenchmarkUnorderedSet_ParallelContain(b *testing.B) {
+	set := NewUnorderedSet[int]()
+	for i := 0; i < 100000; i++ {
+		set.Insert(i)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			set.Contain(i % 100000)
+			i++
+		}
+	})
+}
+
+func BenchmarkUnorderedSet_ParallelRemove(b *testing.B) {
+	set := NewUnorderedSet[int]()
+	for i := 0; i < b.N; i++ {
+		set.Insert(i)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			set.Remove(i)
+			i++
+		}
+	})
+}
